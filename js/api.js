9d708a94c346ab314834a7d3a27bbea65c9c99b7
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2019-05-24 12:18:39 +0200

    Improved readability
    - Made functionnames more descriptive, so the code become more readable
    - Made it so the upScore function doesn't start running until the game-screen is reached

--- js/api.js
+++ js/api.js
@@ -1,17 +1,17 @@
 const imageWidth = 200; //sets the width of the fetched pictures
 const imageHeight = 200; //sets the height of the fetched pictures
-const images = document.getElementsByClassName("genrePic");
-const genres = ["classical music", "rock music", "hiphop music", "game music"]; //sets the search terms 
+const images = document.getElementsByClassName("genreImg");
+const musicTypes = ["classical music", "rock music", "hiphop music", "game music"]; //sets the search terms 
 let x = 0
 
 //a picture is fetched from API for every genre in the genres-array
-for (i=0; i<genres.length; i++){
-fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${genres[i]}`)
-    .then(data => changeSongPic(data))
+for (i=0; i<musicTypes.length; i++){
+fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${musicTypes[i]}`)
+    .then(data => changeSongImg(data))
 }
 
 //fetched pictures are logged one by one on the selectSong screen.
-function changeSongPic(data){   
+function changeSongImg(data){   
     images[x].src = data.url;  
     x++
 }
